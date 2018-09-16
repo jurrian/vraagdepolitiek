c5c76e9f61dec49a7ec2eaf1bbfc602b1d7c95ae
jurrian <jurriantromp@gmail.com>
2018-09-16 16:58:38 +0200

    Init

---
+++ vragenvuur/local_settings.default.py
@@ -0,0 +1,15 @@
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+SECRET_KEY = ''
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
