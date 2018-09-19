c3192eb50c233e83397379463e679351b1e5b62d
jurrian <jurriantromp@gmail.com>
2018-09-19 22:55:25 +0200

    fixup! Serve static files in development

--- vragenvuur/settings.py
+++ vragenvuur/settings.py
@@ -29,7 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'app',
 ]
 
@@ -102,9 +102,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "static"),
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "app", "static"),
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
