5e53e6ac23c31c221279a1cd4aa228a5a8470a69
jurrian <jurriantromp@gmail.com>
2018-09-30 17:47:11 +0200

    Fix for post_migrate created publish_question permission

--- backend/migrations/0010_moderator_permissions.py
+++ backend/migrations/0010_moderator_permissions.py
@@ -3,13 +3,23 @@ from django.db import migrations
 
 def create_moderators_group(apps, schema_editor):
     """Give all permissions on Question except delete."""
+
+    ContentType = apps.get_model('contenttypes', 'ContentType')
+    Question = apps.get_model('backend', 'Question')
+
+    content_type = ContentType.objects.get_for_model(Question)
+
     Group = apps.get_model('auth', 'Group')
     Permission = apps.get_model('auth', 'Permission')
 
     add_question = Permission.objects.get(codename='add_question')
     view_question = Permission.objects.get(codename='view_question')
     change_question = Permission.objects.get(codename='change_question')
-    publish_question = Permission.objects.get(codename='publish_question')
+    publish_question, _ = Permission.objects.get_or_create(
+        codename='publish_question',
+        name='Can publish question',
+        content_type=content_type,
+    )
 
     group = Group.objects.create(pk=1, name='Moderators')
     group.permissions.add(add_question, view_question, change_question,
